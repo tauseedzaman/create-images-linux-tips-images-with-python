f29bd87a6eee682feb48ffd239a587bee4a6fe29
tauseedzaman <tauseedzamanofsorana@gmail.com>
2022-05-20 05:56:23 +0500

    new images added

--- create_image.py
+++ create_image.py
@@ -82,9 +82,9 @@ if __name__ == '__main__':
         bg_image_name=('src/background-'+str(randint(4,17))+'.jpg')
         background = Image.open(bg_image_name)
         l=len(command["description"])
-        if i < 13:
+        if i < 16:
             continue
-        if i >= 16:
+        if i >= 24:
             break
         if l >= 40:
             description=(command["description"].split())
